7300aa516a99e2d7f593bbb9bbcde7a5c22d8d1e
Jaro <38509407+cc-jaro@users.noreply.github.com>
2024-08-14 09:56:17 +0100

    Making the schema and experimenting with some other data exploration and visualisation in prep for the final thesis

---
+++ visualisations/minor visualisation.py
@@ -0,0 +1,65 @@
+import gc
+import time
+import os
+import datetime
+import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy.stats import spearmanr, kruskal
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+import graphviz
+from IPython.display import Image
+
+
+# setting pandas display options
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+# load csv function
+def load_csv(file_name):
+    print(f'loading {file_name}')
+    return pd.read_csv(file_name, low_memory=False)
+
+def load_csv2(file_name, subdirectory='output_files'):
+    file_path = os.path.join(target_directory, subdirectory, file_name)
+    print(f'loading {file_path}')
+    return pd.read_csv(file_path, low_memory=False)
+
+def load_csv3(file_name):
+    print(f'loading header of {file_name}')
+    return pd.read_csv(file_name, nrows=0)
+
+
+
+target_directory = r'C:\Users\ander\Documents\.Uni\Project\mimic-iii-clinical-database-1.4\.unpacked'
+folder_path = r'C:\Users\ander\Documents\.Uni\Project\mimic-iii-clinical-database-1.4\.unpacked\data_exploration'
+
+target_file = 'oecd healthcare spending.csv'
+path = os.path.join(target_directory, target_file)
+os.chdir(target_directory)
+
+
+ref = load_csv2('000_reference_data.csv')
+age = ref[['age']]
+
+adm = load_csv2('111_admissions.csv')
+adm = adm[['HADM_ID','GENDER','INSURANCE','RELIGION','insurance_type','religion_group','age_range']]
+
+pre = load_csv2('316_prescriptions.csv')
+
+
+
+dat = pd.concat([age, adm], axis=1)
+dat = dat[['HADM_ID', 'age', 'age_range','GENDER','INSURANCE','RELIGION','insurance_type','religion_group']]
+
+dat2 = pre[['HADM_ID', 'ROUTE', 'time_since_admit', 'seq_num', 'unique_ndc', 'unique_route_per_hadm_id', 'total_ndc',
+            'mean_dose_val_rx', 'max_dose_val_rx', 'total_dose_val_rx', 'cumulative_dose_val_rx']]
+
+
+
+
+print(dat.sample(6))
+
+print(dat2.sample(30))
